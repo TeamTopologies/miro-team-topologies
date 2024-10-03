8de06aad43bba87d359b4d2e06780f35e553cc1b
João Rosa <joaoasrosa@gmail.com>
2024-10-03 15:37:18 +0200

    Fix typos.

--- src/team-text/team-attention.ts
+++ src/team-text/team-attention.ts
@@ -3,13 +3,13 @@ enum TEAM_ATTENTIONS {
   ManyCollaboration = 1,
   ComplicatedSubsystem,
 }
-// INFO: This is SAMPLE data that needs to be developped AND refined according to official TT content.
+// INFO: This is SAMPLE data that needs to be developed AND refined according to official TT content.
 const teamAttention: string[] = []
 teamAttention[TEAM_ATTENTIONS.SingleCollaboration] =
   'While collaboration enable faster innovation, it also blurry the scope of each teams. Consider using collaboration for fixed durations only.'
 teamAttention[TEAM_ATTENTIONS.ManyCollaboration] =
   'Collaboration increase cognitive load: try avoiding multiple collaborations at a given time.'
 teamAttention[TEAM_ATTENTIONS.ComplicatedSubsystem] =
-  'Complicated Sub-system teams are for very specific situations. Carefuly cross-check if your context does not suit other team types before applying a Complicated Sub-system.'
+  'Complicated Sub-system teams are for very specific situations. Carefully cross-check if your context does not suit other team types before applying a Complicated Sub-system.'
 
 export default teamAttention
