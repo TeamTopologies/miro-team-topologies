57345383c7ddb7da8f7104ec180cd6d969bf3801
João Rosa <joaoasrosa@gmail.com>
2024-10-03 14:56:28 +0200

    Reformat the code to be readable.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
 import {PLUGIN_TITLE, LOGO_TT} from 'config'
 
 miro.onReady(async () => {
-  miro.initialize({
-    extensionPoints: {
-      toolbar: async () => {
-        const permissions = await miro.currentUser.getCurrentBoardPermissions()
-        const canEdit = permissions.includes('EDIT_CONTENT')
+    miro.initialize({
+        extensionPoints: {
+            toolbar: async () => {
+                const permissions = await miro.currentUser.getCurrentBoardPermissions()
+                const canEdit = permissions.includes('EDIT_CONTENT')
 
-        if (canEdit) {
-          return {
-            title: PLUGIN_TITLE,
-            svgIcon: LOGO_TT,
-            librarySvgIcon: LOGO_TT,
-            toolbarSvgIcon: LOGO_TT,
-            onClick: async () => {
-              const authorized = await miro.isAuthorized()
-              if (!authorized) {
-                await miro.requestAuthorization()
-              }
-              miro.board.ui.openLibrary('content-panel.html', {title: PLUGIN_TITLE})
+                if (canEdit) {
+                    return {
+                        title: PLUGIN_TITLE,
+                        svgIcon: LOGO_TT,
+                        librarySvgIcon: LOGO_TT,
+                        toolbarSvgIcon: LOGO_TT,
+                        onClick: async () => {
+                            const authorized = await miro.isAuthorized()
+
+                            if (!authorized) {
+                                await miro.requestAuthorization()
+                            }
+
+                            miro.board.ui.openLibrary(
+                                'content-panel.html',
+                                {
+                                    title: PLUGIN_TITLE
+                                }
+                            )
+                        },
+                    }
+                }
             },
-          }
-        }
-      },
-    },
-  })
+        },
+    })
 })
