c69bbd7008a1e441a4326a1bc629efd4f49f9dc3
João Rosa <joaoasrosa@gmail.com>
2024-10-03 15:09:00 +0200

    Reformat the code to be readable.

--- src/content-panel.tsx
+++ src/content-panel.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-
 import SVG from 'react-inlinesvg'
 
 import {TEAM_ENUM, TeamElement} from './team-logic/team-static'
@@ -17,129 +16,130 @@ require('./styles-miro.css')
 require('./styles.css')
 
 class Root extends React.Component {
-  private containerRef: any = React.createRef()
-  private viewportScale = 1
-  private teamFactory: TeamFactory
+    private containerRef: any = React.createRef()
+    private viewportScale = 1
+    private teamFactory: TeamFactory
 
-  constructor(props: any) {
-    super(props)
-    this.teamFactory = new TeamFactory()
-  }
+    constructor(props: any) {
+        super(props)
+        this.teamFactory = new TeamFactory()
+    }
 
-  // Needed to get scale before previewing drag&drop. As D&D callback is not async.
-  updateCurrentScale = () => {
-    miro.board.viewport.getScale().then((scale) => {
-      this.viewportScale = scale
-    })
-  }
+    // Needed to get scale before previewing drag&drop. As D&D callback is not async.
+    updateCurrentScale = () => {
+        miro.board.viewport.getScale().then((scale) => {
+            this.viewportScale = scale
+        })
+    }
 
-  componentDidMount(): void {
-    // Add drag-and-drop for hotspot
-    const dndOption = {
-      dragDirection: 'vertical',
-      draggableItemSelector: '.draggable-team',
-      getDraggableItemPreview: (targetElement: HTMLElement) => {
-        const teamSize = this.teamFactory.getTeamElementFromClassList(targetElement.classList).getShapeSize()
-        const url = this.teamFactory.getTeamElementFromClassList(targetElement.classList).getPreview()
+    componentDidMount(): void {
+        // Add drag-and-drop for hotspot
+        const dndOption = {
+            dragDirection: 'vertical',
+            draggableItemSelector: '.draggable-team',
+            getDraggableItemPreview: (targetElement: HTMLElement) => {
+                const teamSize = this.teamFactory.getTeamElementFromClassList(targetElement.classList).getShapeSize()
+                const url = this.teamFactory.getTeamElementFromClassList(targetElement.classList).getPreview()
 
-        return {
-          width: 1.3 * this.viewportScale * teamSize.width,
-          height: 1.3 * this.viewportScale * teamSize.height,
-          url: url,
+                return {
+                    width: 1.3 * this.viewportScale * teamSize.width,
+                    height: 1.3 * this.viewportScale * teamSize.height,
+                    url: url,
+                }
+            },
+            onDrop: (canvasX: number, canvasY: number, targetHtml: HTMLElement) => {
+                const teamElement = this.teamFactory.getTeamElementFromClassList(targetHtml.classList)
+                this.createTeamWidget(teamElement, {x: canvasX, y: canvasY})
+            },
         }
-      },
-      onDrop: (canvasX: number, canvasY: number, targetHtml: HTMLElement) => {
-        const teamElement = this.teamFactory.getTeamElementFromClassList(targetHtml.classList)
-        this.createTeamWidget(teamElement, {x: canvasX, y: canvasY})
-      },
+        miro.board.ui.initDraggableItemsContainer(this.containerRef.current, dndOption)
     }
-    miro.board.ui.initDraggableItemsContainer(this.containerRef.current, dndOption)
-  }
 
-  private createTeamWidget = async (teamElement: TeamElement, pos?: {x: number; y: number}) => {
-    const teamShapeSize = teamElement.getShapeSize()
-    if (!pos) {
-      const viewport = await miro.board.viewport.getViewport()
-      pos = {
-        x: viewport.x + viewport.width / 2 - teamShapeSize.width / 2,
-        y: viewport.y + viewport.height / 2 - teamShapeSize.height / 2,
-      }
+    private createTeamWidget = async (teamElement: TeamElement, pos?: { x: number; y: number }) => {
+        const teamShapeSize = teamElement.getShapeSize()
+        if (!pos) {
+            const viewport = await miro.board.viewport.getViewport()
+            pos = {
+                x: viewport.x + viewport.width / 2 - teamShapeSize.width / 2,
+                y: viewport.y + viewport.height / 2 - teamShapeSize.height / 2,
+            }
+        }
+        await miro.board.widgets.create({
+            metadata: {
+                [miro.getClientId()]: {
+                    teamtopology: true,
+                    teamEnum: TEAM_ENUM[teamElement.getTeamEnum()],
+                },
+            },
+            type: 'SHAPE',
+            x: pos.x,
+            y: pos.y,
+            style: teamElement.getStyle(),
+            createdUserId: '',
+            lastModifiedUserId: '',
+            width: teamShapeSize.width,
+            height: teamShapeSize.height,
+            rotation: 0,
+            text: teamElement.getName(),
+        })
     }
-    await miro.board.widgets.create({
-      metadata: {
-        [miro.getClientId()]: {
-          teamtopology: true,
-          teamEnum: TEAM_ENUM[teamElement.getTeamEnum()],
-        },
-      },
-      type: 'SHAPE',
-      x: pos.x,
-      y: pos.y,
-      style: teamElement.getStyle(),
-      createdUserId: '',
-      lastModifiedUserId: '',
-      width: teamShapeSize.width,
-      height: teamShapeSize.height,
-      rotation: 0,
-      text: teamElement.getName(),
-    })
-  }
 
-  renderTeamElement(teamElement: TeamElement) {
-    return (
-      <div
-        key={teamElement.getTeamEnum()}
-        className={'draggable-team ' + teamElement.getClassName()}
-        title={teamElement.getName()}
-        onClick={() => this.createTeamWidget(teamElement)}
-        onMouseEnter={() => {
-          if (this.setDetailText != undefined) this.setDetailText(teamElement.getTeamEnum())
-        }}
-      >
-        <SVG className="icon" src={teamElement.getIcon()} />
-      </div>
-    )
-  }
+    renderTeamElement(teamElement: TeamElement) {
+        return (
+            <div
+                key={teamElement.getTeamEnum()}
+                className={'draggable-team ' + teamElement.getClassName()}
+                title={teamElement.getName()}
+                onClick={() => this.createTeamWidget(teamElement)}
+                onMouseEnter={() => {
+                    if (this.setDetailText != undefined) this.setDetailText(teamElement.getTeamEnum())
+                }}
+            >
+                <SVG className="icon" src={teamElement.getIcon()}/>
+            </div>
+        )
+    }
 
-  render() {
-    const teamContent = (
-      <div className="tt_main_container" onMouseEnter={this.updateCurrentScale}>
-        <h4 className="sub-title">Team types:</h4>
-        <h5 className="sub-title">Fundamental:</h5>
-        <div className="team-types">
-          {TeamType.TeamEnums.map((teamEnum) => {
-            return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
-          })}
-        </div>
-        <h5 className="sub-title">Supplementary:</h5>
-        <div className="team-supplementary">
-          {TeamSupplementary.TeamEnums.map((teamEnum) => {
-            return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
-          })}
-        </div>
-        <h4 className="sub-title">Team interactions:</h4>
-        <div className="team-interactions">
-          {TeamInteraction.TeamEnums.map((teamEnum) => {
-            return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
-          })}
-        </div>
-        <div className="team-other">
-          <h4 className="sub-title">Flow of change:</h4>
-          {TeamOther.TeamEnums.map((teamEnum) => {
-            return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
-          })}
-        </div>
-        <DetailsPanel setOnHover={this.setOnHover} />
-      </div>
-    )
-    return <div ref={this.containerRef}>{teamContent}</div>
-  }
-  private setOnHover = (callBack: (teamEnum: TEAM_ENUM) => void) => {
-    this.setDetailText = callBack
-  }
-  private setDetailText: ((teamEnum: TEAM_ENUM) => void) | undefined = undefined
+    render() {
+        const teamContent = (
+            <div className="tt_main_container" onMouseEnter={this.updateCurrentScale}>
+                <h4 className="sub-title">Team types:</h4>
+                <h5 className="sub-title">Fundamental:</h5>
+                <div className="team-types">
+                    {TeamType.TeamEnums.map((teamEnum) => {
+                        return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
+                    })}
+                </div>
+                <h5 className="sub-title">Supplementary:</h5>
+                <div className="team-supplementary">
+                    {TeamSupplementary.TeamEnums.map((teamEnum) => {
+                        return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
+                    })}
+                </div>
+                <h4 className="sub-title">Team interactions:</h4>
+                <div className="team-interactions">
+                    {TeamInteraction.TeamEnums.map((teamEnum) => {
+                        return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
+                    })}
+                </div>
+                <div className="team-other">
+                    <h4 className="sub-title">Flow of change:</h4>
+                    {TeamOther.TeamEnums.map((teamEnum) => {
+                        return this.renderTeamElement(this.teamFactory.getTeamElement(teamEnum))
+                    })}
+                </div>
+                <DetailsPanel setOnHover={this.setOnHover}/>
+            </div>
+        )
+        return <div ref={this.containerRef}>{teamContent}</div>
+    }
+
+    private setOnHover = (callBack: (teamEnum: TEAM_ENUM) => void) => {
+        this.setDetailText = callBack
+    }
+    private setDetailText: ((teamEnum: TEAM_ENUM) => void) | undefined = undefined
 }
 
 miro.onReady(() => {
-  ReactDOM.render(<Root />, document.getElementById('content-panel-react-app'))
+    ReactDOM.render(<Root/>, document.getElementById('content-panel-react-app'))
 })
