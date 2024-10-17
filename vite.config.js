593c483712849f12ba72f751cbddeda2a8d8d5d2
Richard Allen <richard.allen@conjurersolutions.co.uk>
2024-10-17 22:18:42 +0100

    Add missing vite config entry

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import path from 'path';
-import fs from 'fs';
-import dns from 'dns';
-import {defineConfig} from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from "path";
+import fs from "fs";
+import dns from "dns";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/server-options.html#server-host
-dns.setDefaultResultOrder('verbatim');
+dns.setDefaultResultOrder("verbatim");
 
 // make sure vite picks up all html files in root, needed for vite build
 const allHtmlEntries = fs
-  .readdirSync('.')
-  .filter((file) => path.extname(file) === '.html')
+  .readdirSync(".")
+  .filter((file) => path.extname(file) === ".html")
   .reduce((acc, file) => {
-    acc[path.basename(file, '.html')] = path.resolve(__dirname, file);
+    acc[path.basename(file, ".html")] = path.resolve(__dirname, file);
 
     return acc;
   }, {});
@@ -22,6 +22,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: allHtmlEntries,
+      external: ["/src/App.tsx"],
     },
   },
   plugins: [reactRefresh()],
